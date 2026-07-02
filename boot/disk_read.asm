2937e28e56ea1efa20449abef89be515f87eee0d
vinicius-arg <j.vin7cius@gmail.com>
2026-07-01 21:38:54 -0300

    feat: Disk reading

---
+++ boot/disk_read.asm
@@ -0,0 +1,21 @@
+read_sector:
+    mov ah, 0x02
+    mov al, 0x01
+    int 0x13
+    jc .fail
+
+    mov si, disk_read
+    call print_string
+
+    ret
+
+.fail:
+    mov si, read_error_msg
+    call print_string
+    jmp $
+
+
+disk_read db   "Reading disk at 0x0000", 0
+read_error db  "Disk read error.", 0
+
+%include "./boot/print.asm"
